e15543cf6e5327d92b54e055dca835868ea495d4
kwan528 <kwan772@aucklanduni.ac.nz>
2023-08-12 20:51:38 +1200

    ml models

--- ml_model/lstm_model_sentiment.py
+++ ml_model/lstm_model_sentiment.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
@@ -9,18 +11,62 @@ from tensorflow.keras.callbacks import TensorBoard
 import yfinance as yf
 from sklearn.metrics import mean_squared_error
 import datetime
+from sqlalchemy import create_engine, text
 
-# Fetch data for Tesla over a specific period (e.g., the past 5 years)
-tesla = "TSLA"
+# Create a connection to your MySQL database
+db_connection_str = 'mysql+pymysql://root:' + os.getenv('DB_PASSWORD') + '@localhost/p4p'
+engine = create_engine(db_connection_str)
+
+query = f"""
+SELECT 
+COUNT(*) AS total_comments,
+(SUM(CASE WHEN sentiment = 'positive' THEN 1 ELSE 0 END) * 1.0) AS positive_sentiment_ratio,
+(SUM(CASE WHEN sentiment = 'negative' THEN 1 ELSE 0 END) * 1.0) AS negative_sentiment_ratio,
+date(from_unixtime(created_utc)) d FROM p4p.comments_for_certain_symbols where symbol = "tsla" group by date(from_unixtime(created_utc));
+"""
+
+sentiment_df = None
 start_date = "2020-01-01"
 end_date = "2023-01-01"
-df = yf.download(tesla, start=start_date, end=end_date)
 
-pd.set_option("display.max_columns", None)
+with engine.connect() as conn:
+    sentiment_df = pd.read_sql(text(query), conn)
+
+sentiment_df.set_index("d", inplace=True)
+sentiment_df.index = pd.to_datetime(sentiment_df.index)
+sentiment_df.index = sentiment_df.index + pd.DateOffset(days=1)
+sentiment_df = sentiment_df.sort_index()[start_date:end_date]
+
+# print(sentiment_df)
+
+tesla = "TSLA"
+
+# Fetch data
+df = yf.download(tesla, start=start_date, end=end_date)
+# df = df['2017-12-30':'2023-01-02']
+
+# pd.set_option("display.max_columns", None)
+
+# print(df)
+total_count = 0
+zero_count = 0
+
+result_df = df.join(sentiment_df, how='left').fillna(0.0)
+for index,row in result_df.iterrows():
+    if row['total_comments'] == 0:
+        # print(row)
+        zero_count += 1
+    total_count+=1
+
+# print(zero_count)
+# print(total_count)
+
 # Use High, Low, Open, Close, and Volume for training the model.
-data = df[['High', 'Low', 'Open', 'Close', 'Volume']].values
+data = result_df[['High', 'Low', 'Open', 'Close', 'Volume', 'positive_sentiment_ratio',
+           'negative_sentiment_ratio']].values
 scaler = MinMaxScaler()
 scaled_data = scaler.fit_transform(data)
+num_feature = 7
 
 # Prepare data in sequences
 X, y = [], []
@@ -29,14 +75,15 @@ for i in range(60, len(scaled_data)):
     y.append(scaled_data[i, 3])  # Assuming 'Close' price is the target
 
 X, y = np.array(X), np.array(y)
-X = np.reshape(X, (X.shape[0], X.shape[1], 5))  # Updated to consider 5 features
+X = np.reshape(X, (X.shape[0], X.shape[1], num_feature))  # Updated to consider 5 features
 # Split data
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, shuffle=False)
 
 # Define the LSTM model
 model = Sequential()
-model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 5)))
-model.add(LSTM(units=50))
+model.add(LSTM(units=50, input_shape=(X_train.shape[1], num_feature)))
+model.add(Dense(units=20, activation="relu"))
+# model.add(Dense(units=20, activation="relu"))
 model.add(Dense(units=1))
 
 model.compile(optimizer='adam', loss='mean_squared_error')
@@ -51,12 +98,12 @@ model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_val, y_
 predicted_prices = model.predict(X_val)
 
 # Create a dummy array for inverse transformation of predictions
-dummy_array_predictions = np.zeros((len(predicted_prices), 5))
+dummy_array_predictions = np.zeros((len(predicted_prices), num_feature))
 dummy_array_predictions[:, 3] = predicted_prices[:, 0]
 inverse_transformed_predictions = scaler.inverse_transform(dummy_array_predictions)[:, 3]
 
 # Create a dummy array for inverse transformation of actual prices
-dummy_array_actual = np.zeros((len(y_val), 5))
+dummy_array_actual = np.zeros((len(y_val), num_feature))
 dummy_array_actual[:, 3] = y_val
 inverse_transformed_actual = scaler.inverse_transform(dummy_array_actual)[:, 3]
 
@@ -75,5 +122,4 @@ rmse = np.sqrt(mse)
 print(f"Root Mean Squared Error: {rmse}")
 
 # Save the model
-model.save("tesla_lstm_model.keras")
-
+model.save("tesla_lstm_model_sentiment.keras")
